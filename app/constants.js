861e7217a209ac8491943d3346fc18ed00c29e5d
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-05-31 21:55:18 +0530

    added functionality to connect to a database

--- app/constants.js
+++ app/constants.js
@@ -1,2 +1,9 @@
 // Constants
-module.exports = {};
+module.exports = {
+  DB: {
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "password",
+    DBNAME: process.env.DB_NAME || "lk-billboards"
+  }
+};
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 // Modules
 const express = require("express");
-const serverPort = process.env.PORT || 8000;
+const mysql = require("mysql");
 const app = express();
 
-// Start Server
-app.listen(serverPort, () => {
-  console.log(`Live on ${serverPort}`);
+// Properties
+const serverPort = process.env.PORT || 8000;
+const constants = require("./app/constants");
+
+// Configure Database Connection
+const connection = mysql.createConnection({
+  host: constants.DB.HOSTNAME,
+  user: constants.DB.USER,
+  password: constants.DB.PASSWORD,
+  database: constants.DB.DBNAME
+});
+
+// Establish connection with database
+connection.connect(error => {
+  if (error) return console.log(error);
+
+  // Start Server
+  app.listen(serverPort, () => {
+    return console.log(`Live on ${serverPort}`);
+  });
 });
