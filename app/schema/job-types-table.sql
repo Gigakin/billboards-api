edc6d33309b8361ece96b8db72d821350d894789
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-06-03 10:00:52 +0530

    added tables schema (incomplete)

--- app/schema/job-types-table.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists jobtypes (
-  id int(10) primary key auto_increment,
-  job_type_name varchar(32),
-  foreign key (type_feature_id)
-    references typefeatures(id)
-) Engine=InnoDB;
