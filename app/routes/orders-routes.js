55f6fd9bbbf124c93b069e4f5b950966fc559e56
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-07-04 09:28:23 +0530

    formatted code

--- app/routes/orders-routes.js
+++ app/routes/orders-routes.js
@@ -11,28 +11,13 @@ module.exports = (app, dbInstance, upload) => {
   app.post("/api/orders/:id/status", ordersController.changeOrderStatus);
   app.delete("/api/orders/:id", ordersController.deleteOrder);
   app.post("/api/orders/:id/jobs", ordersController.addJobs);
-  app.post(
-    "/api/orders/:id/jobs/advance",
-    ordersController.setJobAdvanceAmounts
-  );
+  app.post("/api/orders/:id/jobs/advance", ordersController.setJobAdvanceAmounts);
   app.delete("/api/orders/:orderid/jobs/:jobid", ordersController.removeJob);
 
   // Jobs Files
-  app.post(
-    "/api/orders/:orderid/jobs/:jobid/files/customer",
-    upload.single("file"),
-    ordersController.saveCustomerFile
-  );
-  app.post(
-    "/api/orders/:orderid/jobs/:jobid/files/designer",
-    upload.single("file"),
-    ordersController.saveDesignerFile
-  );
-  app.post(
-    "/api/orders/:orderid/jobs/:jobid/files/printer",
-    upload.single("file"),
-    ordersController.savePrinterFile
-  );
+  app.post("/api/orders/:orderid/jobs/:jobid/files/customer", upload.single("file"), ordersController.saveCustomerFile);
+  app.post("/api/orders/:orderid/jobs/:jobid/files/designer", upload.single("file"), ordersController.saveDesignerFile);
+  app.post("/api/orders/:orderid/jobs/:jobid/files/printer", upload.single("file"), ordersController.savePrinterFile);
 
   // Handovers
   app.post("/api/orders/:id/jobs/handovers", ordersController.handoverJobs);
