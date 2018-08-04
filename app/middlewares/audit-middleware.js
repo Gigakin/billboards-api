242d22704a56f05a7629f4587dc71efd036a4ec2
Ajay Gupta <ajay.gupta8308@gmail.com>
2018-08-04 09:09:42 +0530

    can log userid when performing an action and not logging get reqs

--- app/middlewares/audit-middleware.js
+++ app/middlewares/audit-middleware.js
@@ -1,3 +1,9 @@
+// Modules
+const jwt = require("jsonwebtoken");
+
+// Imports
+const Constants = require("../constants");
+
 // Set Database Instance
 let database = null;
 let setDbInstance = instance => {
@@ -9,9 +15,25 @@ let auditRequests = (request, response, next) => {
   if (database) {
     let { method, url, headers } = request;
 
+    // Do not log get requests
+    // Unnecessarily increases db size
+    if (method && method === "GET") {
+      return next();
+    }
+
+    // Extract User ID
+    let userid = 0;
+    if (headers && headers.authorization) {
+      let decodedToken = jwt.decode(
+        headers.authorization,
+        Constants.JWT.SECRET_KEY
+      );
+      userid = decodedToken.id;
+    }
+
     // TODO: Extract User ID and
     // replace it with 0 in dbVaulues
-    let dbValues = [0, `"${method}"`, `"${url}"`, `"${headers.host}"`];
+    let dbValues = [userid, `"${method}"`, `"${url}"`, `"${headers.host}"`];
 
     // Insert into DB
     database.query(
